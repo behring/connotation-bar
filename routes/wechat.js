da594962f1fe46e6145356f294b472ebe6a36c12
behring <zhaolin53636848@126.com>
2017-05-15 23:04:46 +0800

    fix bug

--- routes/wechat.js
+++ routes/wechat.js
@@ -11,7 +11,7 @@ let config = {
   checkSignature: false // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
-router.get('/', wechat(config, function (req, res, next) {
+router.use('/', wechat(config, function (req, res, next) {
   // 微信输入信息都在req.weixin上
   let message = req.weixin;
   if (message.FromUserName === 'diaosi') {
