3ef7934b82a7cce59686b28f68ed09de740c98e1
behring <zhaolin53636848@126.com>
2017-05-17 13:30:05 +0800

    定时任务添加到代码中

--- cloud.js
+++ cloud.js
@@ -3,6 +3,18 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!';
+AV.Cloud.define('resetVisitedCartoonCount', function(request) {
+  var query = new AV.Query('WechatUser');
+  query.find().then(function (users) {
+    users.forEach(function(user) {
+      user.set('visitedCartoonCount', 0);
+    });
+    return AV.Object.saveAll(users);
+  }).then(function(users) {
+    // 更新成功
+    console.log("visitedCartoonCount 更新成功");
+  }, function (error) {
+    // 异常处理
+    console.error(error);
+  });
 });
