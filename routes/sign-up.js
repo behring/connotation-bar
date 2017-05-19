946a30408f33578344bafe779454ebe84c882241
behring <zhaolin53636848@126.com>
2017-05-19 14:08:28 +0800

    完成注册页面UI

--- routes/sign-up.js
+++ routes/sign-up.js
@@ -7,7 +7,7 @@ let Cartoon = require('.././models/cartoon');
 
 router.get('/', function(req, res) {
     Cartoon.pagination(1,24).then(cartoons => {
-        res.render('home/index', { cartoons: cartoons });
+        res.render('sign-up/sign-up', { cartoons: cartoons });
     });
 
 });
