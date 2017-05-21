7497af776452e07b0817bba1998f18cdb126b9af
behring <zhaolin53636848@126.com>
2017-05-21 12:26:42 +0800

    详情增加上一页下一页

--- routes/pictures.js
+++ routes/pictures.js
@@ -20,10 +20,6 @@ router.get('/', function(req, res) {
 router.get('/:category/:number', function(req, res, next) {
     let category = req.params.category;
     let number = parseInt(req.params.number);
-
-    console.log(category);
-    console.log(number);
-
     res.format({
         'text/html': function(){
             Picture.queryOneBy({category, number}).then(picture => {
